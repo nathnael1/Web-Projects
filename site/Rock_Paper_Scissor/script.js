7534abbe7da6d48136f3a3eb639a4bc1fce582d1
nathnael1 <81173345+nathnael1@users.noreply.github.com>
2024-01-08 21:19:02 +0300

    Update script.js

--- site/Rock_Paper_Scissor/script.js
+++ site/Rock_Paper_Scissor/script.js
@@ -233,7 +233,7 @@ if(gettingValue.round==1){
 
 })
 }
-const duration = 6850;
+const duration = 6900;
 const alwaysH2 = document.getElementById("always");
 setTimeout(function() {
     document.getElementById("loading-container").style.display = "none";
