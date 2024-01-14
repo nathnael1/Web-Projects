d216e61b8ff2072c01faeff2f798763b1c6caed3
nathnael1 <81173345+nathnael1@users.noreply.github.com>
2024-01-14 13:46:51 +0300

    Update script.js

--- site/Tic _Tac_Toe/script.js
+++ site/Tic _Tac_Toe/script.js
@@ -196,9 +196,9 @@ if(gaming.checked==0){
                         y.classList.remove("disabled");
                         y.classList.add("circle");
                         box.classList.add("ochecked");
-                        document.getElementById("a").style.display="block"
-                        document.getElementById("b").style.display="block"
-                        document.getElementById("c").style.display="block"
+                        document.getElementById("a").style.display="none"
+                        document.getElementById("b").style.display="none"
+                        document.getElementById("c").style.display="none"
                         
                         counter++;
                         winingConditionO();
